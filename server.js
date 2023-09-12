e3567e1b12bc84df781eaae373d337aa39a49540
RimmaO <rimma_o@ukr.net>
2023-09-12 13:07:43 +0300

    init project

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://rimma:LBkXCvvy7td7KDxU@cluster0.ehv8x6h.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+const app = require("./app");
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
